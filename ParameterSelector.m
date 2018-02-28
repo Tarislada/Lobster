c0de77f8ebe5428568fd57686b3eded72aa77a7d
Knowblesse <knowblesse@gmail.com>
2018-02-28 18:06:43 +0900

    주석 추가. trail 별로 분석해야하는 부분을 한데로 묶음. Inter Attack IROF Interval 구하는 공식 추가함.

--- ParameterSelector.m
+++ ParameterSelector.m
@@ -8,13 +8,14 @@ cumLicks = [];
 cumAttacks = [];
 cumILickI= [];
 cumIIRI= [];
+cumIAttackIROFI = [];
 
-for i = {'lob1','lob2','lob3','lob4'}
+for i = {'lob1','lob2','lob3','lob4'} % 이 폴더륻ㄹ에 대해서
     datasetdir = dir(strcat(cd,'\data\',cell2mat(i)));
     datasetdir = datasetdir(3:end);
     datasetnames = struct2cell(datasetdir);
         
-    for j = 1 : size(datasetdir,1)
+    for j = 1 : size(datasetdir,1) % 모든 subject 데이터에 대해
         location = strcat(cd,'\data\',i,'\',datasetnames{1,j});
         [ParsedData, Trials, IRs, Licks,Attacks ] = BehavDataParser(cell2mat(location));
         cumTrials = [cumTrials;Trials];
@@ -22,21 +23,37 @@ for i = {'lob1','lob2','lob3','lob4'}
         cumLicks = [cumLicks; Licks];
         cumAttacks = [cumAttacks; Attacks];
         
-       %% IR 간격 분석
+       
         IIRI = [];
-        for trial = 1 : size(Trials,1)
-            IRInInTrial = ParsedData{trial,2};
-            IIRI = [IIRI;IRInInTrial(2:end,1) - IRInInTrial(1:end-1,2)];
-        end 
-        cumIIRI = [cumIIRI;IIRI];
-        
-       %% Lick 간격 분석
         ILickI = [];
+        IAttackIROFI = [];
+        
         for trial = 1 : size(Trials,1)
+            %% IR 간격 분석
+            IRInTrial = ParsedData{trial,2};
+            IIRI = [IIRI;IRInTrial(2:end,1) - IRInTrial(1:end-1,2)];
+            %% Lick 간격 분석
             LicksInTrial = ParsedData{trial,3};
             ILickI = [ILickI;LicksInTrial(2:end,1) - LicksInTrial(1:end-1,2)];
+            %% Inter Attack-IROFF Interval
+            if isempty(IRInTrial)
+                continue;
+            elseif isempty(ParsedData{trial,4})
+                continue;
+            else
+                % 해당 trial 에서 Attack 시간보다 IR 값의 마지막 값
+                % (=마지막 IR의 On/Off 값 중Off 값)이 더 크다면,
+                % 맞으면서(Attack 먼저) 빼는(IR Off) 케이스.
+                % 그러나 간혹 Attack 이 다 끝나고 Trial을 끝내기전에
+                % 얼굴을 들이밀어 IR이 끊기는 경우가 있다.
+                % 이런 경우는 맨 마지막 IR이 아니라 그 전 IR Off와 
+                % Inter-Attack-IROF-Interval을 계산해야한다.
+                %if IRInTrial(end) - ParsedData{trial,4} > 1
+                IAttackIROFI = [IAttackIROFI;IRInTrial(end) - ParsedData{trial,4}];
+            end 
         end 
+        cumIIRI = [cumIIRI;IIRI];
         cumILickI = [cumILickI;ILickI];
-
+        cumIAttackIROFI = [cumIAttackIROFI; IAttackIROFI];
     end
 end
