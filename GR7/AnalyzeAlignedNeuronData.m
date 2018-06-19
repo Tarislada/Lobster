28962f6bbde70e7d6b25b65f464335cdf4faf283
Knowblesse <knowblesse@gmail.com>
2018-06-19 17:01:45 +0900

    MakeBigArray 이름 변경 및 주석처리

---
+++ GR7/AnalyzeAlignedNeuronData.m
@@ -0,0 +1,106 @@
+%% 추출된 aligned 된 mat 파일을 읽어서 하나의 큰 파일로 만들기.
+% 그리고 PCA 분석하기.
+
+
+%% 분석할 .mat 파일 선택
+[filename, pathname] = uigetfile('.mat', 'MultiSelect', 'on');
+Paths = strcat(pathname,filename);
+if (ischar(Paths))
+    Paths = {Paths};
+    filename = {filename};
+end
+
+numNeuron = numel(Paths); % 선택한 뉴런의 수.
+
+
+%% 선택한 .mat 파일의 수를 가지고 cell을 만든뒤 하나씩 로드하여 저장.
+Neurons = cell(numNeuron,1); % 로드한 데이터를 담아두는 변수.
+for f = 1 : numNeuron
+    load(Paths{f});
+    Neurons{f} = Z;
+    clearvars Z
+end
+clearvars f;
+
+%% 각 이벤트에 align된 데이터를 가지고 matrix 만들기
+TRON_mat = zeros(numNeuron,numel(Neurons{1}.TRON));
+IRON_mat = zeros(numNeuron,numel(Neurons{1}.IRON));
+LICK_mat = zeros(numNeuron,numel(Neurons{1}.LICK));
+LOFF_mat = zeros(numNeuron,numel(Neurons{1}.LOFF));
+IROF_mat = zeros(numNeuron,numel(Neurons{1}.IROF));
+ATTK_mat = zeros(numNeuron,numel(Neurons{1}.ATTK));
+TROF_mat = zeros(numNeuron,numel(Neurons{1}.TROF));
+
+for n = 1 : numNeuron
+    TRON_mat(n,:) = Neurons{n}.TRON;
+    IRON_mat(n,:) = Neurons{n}.IRON;
+    LICK_mat(n,:) = Neurons{n}.LICK;
+    LOFF_mat(n,:) = Neurons{n}.LOFF;
+    IROF_mat(n,:) = Neurons{n}.IROF;
+    ATTK_mat(n,:) = Neurons{n}.ATTK;
+    TROF_mat(n,:) = Neurons{n}.TROF;
+end
+
+clearvars Neurons n
+
+%% figure 그리기 (sorting 안하고 선택한 neuron___aligned.mat 파일 순서로 plot
+% figure('name','TRON');
+% imagesc(TRON_mat);
+% figure('name','IRON');
+% imagesc(IRON_mat);
+% figure('name','LICK');
+% imagesc(LICK_mat);
+% figure('name','LOFF');
+% imagesc(LOFF_mat);
+% figure('name','IROF');
+% imagesc(IROF_mat);
+% figure('name','ATTK');
+% imagesc(ATTK_mat);
+% figure('name','TROF');
+% imagesc(TROF_mat);
+
+%% Constants
+varnames = {'TRON_mat','IRON_mat','LICK_mat','LOFF_mat','IROF_mat','ATTK_mat','TROF_mat'};
+zeroline = [61,31,61,61,101,101,101]; % A3에서 edges를 기준으로 0인 지점의 index 값. A3 코드가 바뀌면 바꿔줘야 함!!!
+xedges = {-6:0.1:12, -3:.1:10, -6:.1:12, -6:.1:6, -10:.1:5, -10:.1:5, -10:.1:5}; 
+
+N = 4; % 사용하는 Component의 수
+
+%% PCA 돌리고 그 결과값(loading)에 따라서 재정렬)
+Screensize = get(groot, 'Screensize'); % 좌에 정렬된 그래프, 우에 eigenvector를 표시하기 위한 부분.
+
+for events = 1 : numel(varnames)
+    % PCA 돌리기. 이 부분은 논문의 코드와 정확히 일치함.
+    X = eval(varnames{events});
+    [u, s, v] = svd(X',0);
+    U = u(:,1:N); % eigenvenctors
+    l = diag(s).^2/(numNeuron-1); % eigenvalues
+    Z = X*U; % scores = loading
+    clearvars u s v
+    
+    % Component figure 그리기.
+    gui2 = figure('name',[varnames{events}, ' : Components']);
+    set(gui2, 'Position', [1,1,Screensize(3)/2,Screensize(4)]);
+    movegui(gui2,'east');
+    for comp = 1 : N
+        subplot(4,1,comp);
+        plot(U(:,comp));
+    end
+    
+    % loading 값에 따라서 정렬한 Neuron 데이터 그리기.
+    gui1 = figure('name',varnames{events});
+    set(gui1, 'Position', [1,1,Screensize(3)/2,Screensize(4)]);
+    movegui(gui1,'west');
+    % 총 N개의 component를 사용. 각 component의 loading 값을 기준으로 정렬함.
+    % 데이터에는 따로 손을 안대고 정렬하는 방식만 바꾼 것.
+    for comp = 1 : N
+        subplot(4,1,comp);
+        [~,sortindex] = sort(Z(:,comp));
+        imagesc(X(sortindex,:));
+        colormap jet;
+        hold on;
+        line([zeroline(events), zeroline(events)],[0, numNeuron],'Color','r');
+    end
+end
+        
+
