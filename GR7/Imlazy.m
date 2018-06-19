534e8fb0c9b92acb0fbced7c46f8a181919120f5
Knowblesse <knowblesse@gmail.com>
2018-06-19 17:02:04 +0900

    주석처리

--- GR7/Imlazy.m
+++ GR7/Imlazy.m
@@ -1,3 +1,8 @@
+%% A2 스크립트와 A3 스크립트를 돌린 후 그려진 데이터를 한번에 저장하기 위한 script
+% 먼저 uigetfile 함수를 통해서 여러개의 뉴런 데이터(.mat)형식을 선택.
+% 다음으로 GambleRatsBehavParser를 돌리기 위해서 EVENT 폴더를 선택해 주는 uiget dir을 실행.
+% 처음에 선택한 뉴런 데이터의 파일명을 기준으로 A3를 돌린뒤 해당 결과물을 .png 형식으로 저장.
+
 [filename, pathname] = uigetfile('.mat', 'MultiSelect', 'on');
 Paths = strcat(pathname,filename);
 if (ischar(Paths))
--- GR7/Imlazy_createmat.m
+++ GR7/Imlazy_createmat.m
@@ -1,5 +1,8 @@
-% A2와 A3_v3_JH를 동시에 돌리는데, 향후 PCA 분석을 위해서 figure는 그리지 않고 mat 파일만 저장하는
-% 프로그램입니다.
+%% A2 스크립트와 A3 스크립트를 돌린 후 뉴런 firing 데이터만 저장하는 script
+% 먼저 uigetfile 함수를 통해서 여러개의 뉴런 데이터(.mat)형식을 선택.
+% 다음으로 GambleRatsBehavParser를 돌리기 위해서 EVENT 폴더를 선택해 주는 uiget dir을 실행.
+% 처음에 선택한 뉴런 데이터의 파일명을 기준으로 A3를 돌린뒤 해당 결과물을 .png 형식으로 저장.
+% 향후 PCA 분석을 위함.
 
 [filename, pathname] = uigetfile('.mat', 'MultiSelect', 'on');
 Paths = strcat(pathname,filename);
@@ -13,7 +16,7 @@ targetdir = uigetdir();
 for f = 1 : numel(Paths)
     load(Paths{f});
     neuronname = filename{f};
-    donotdrawA2 = true;
+    donotdrawA2 = true; % A2에서 이 파트가 있으면 뒤에서 그래프를 그리지 않음.
     A2_GR_singleUnit_anlyzer_JH;
     A3_GR_PSTH_v3_JH;
     clearvars -except filename pathname Paths targetdir f Z
