254cbd2fa6042e68bd52901fea782314891dd760
Knowblesse <knowblesse@gmail.com>
2018-08-08 17:59:49 +0900

    dead lines are removed

--- GR7/A1_GR_UNITS_read_v2.m
+++ GR7/A1_GR_UNITS_read_v2.m
@@ -1,123 +1,29 @@
-%% 텍스트 파일에서 데이터를 가져옵니다.
-% open units exported in 'txt' file
+%% A1_GR_UNITS_read
+% .txt 형식으로 되어 있는 firing data 파일을 가져와서 neuron별로 .mat 파일을 생성한다.
 
-clear
+%% Initialize
+clear;
 
-
+%% open units exported in 'txt' file
 [FileName,PathName] = uigetfile('.txt');
 
-
-%% reset variables
+% reset variables
 F = strcat(PathName, FileName);
 delimiter = ',';
 startRow = 2;
 
-%% format of each text line
-%   열1: double (%f)
-%	열2: double (%f)
-%   열3: double (%f)
-%	열4: double (%f)
-%   열5: double (%f)
-%	열6: double (%f)
-%   열7: double (%f)
-%	열8: double (%f)
-%   열9: double (%f)
-%	열10: double (%f)
-%   열11: double (%f)
-%	열12: double (%f)
-%   열13: double (%f)
-%	열14: double (%f)
-%   열15: double (%f)
-%	열16: double (%f)
-%   열17: double (%f)
-%	열18: double (%f)
-%   열19: double (%f)
-%	열20: double (%f)
-%   열21: double (%f)
-%	열22: double (%f)
-%   열23: double (%f)
-%	열24: double (%f)
-%   열25: double (%f)
-%	열26: double (%f)
-%   열27: double (%f)
-%	열28: double (%f)
-%   열29: double (%f)
-%	열30: double (%f)
-%   열31: double (%f)
-%	열32: double (%f)
-%   열33: double (%f)
-%	열34: double (%f)
-%   열35: double (%f)
-%	열36: double (%f)
-%   열37: double (%f)
-%	열38: double (%f)
-%   열39: double (%f)
-%	열40: double (%f)
-%   열41: double (%f)
-%	열42: double (%f)
-%   열43: double (%f)
-%	열44: double (%f)
-%   열45: double (%f)
-%	열46: double (%f)
-%   열47: double (%f)
-%	열48: double (%f)
-%   열49: double (%f)
-%	열50: double (%f)
-%   열51: double (%f)
-%	열52: double (%f)
-%   열53: double (%f)
-%	열54: double (%f)
-%   열55: double (%f)
-%	열56: double (%f)
-%   열57: double (%f)
-%	열58: double (%f)
-%   열59: double (%f)
-%	열60: double (%f)
-%   열61: double (%f)
-%	열62: double (%f)
-%   열63: double (%f)
-%	열64: double (%f)
-%   열65: double (%f)
-%	열66: double (%f)
-%   열67: double (%f)
-%	열68: double (%f)
-%   열69: double (%f)
-%	열70: double (%f)
-%   열71: double (%f)
-%	열72: double (%f)
-%   열73: double (%f)
-%	열74: double (%f)
-%   열75: double (%f)
-%	열76: double (%f)
-%   열77: double (%f)
-%	열78: double (%f)
-%   열79: double (%f)
-%	열80: double (%f)
-%   열81: double (%f)
-%	열82: double (%f)
-%   열83: double (%f)
-%	열84: double (%f)
-%   열85: double (%f)
-%	열86: double (%f)
-%   열87: double (%f)
-%	열88: double (%f)
-%   열89: double (%f)
-%	열90: double (%f)
-%   열91: double (%f)
-% 
+% format of each text line : %   열1~열 91 : double (%f)
 formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
 
-%% open txt file
+% open txt file
 fileID = fopen(F,'r');
 
-%% read file
-
+% read file
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'TextType', 'string', 'EmptyValue', NaN, 'HeaderLines' ,startRow-1, 'ReturnOnError', false, 'EndOfLine', '\r\n');
 
-%% close txt file
+% close txt file
 fclose(fileID);
-
-%% 
+ 
 %% make output variables
 UNITS = table(dataArray{1:end-1}, 'VariableNames', {'Timestamp','Channel','Unit','VarName4','VarName5','VarName6','VarName7','VarName8','VarName9','VarName10','VarName11','VarName12','VarName13','VarName14','VarName15','VarName16','VarName17','VarName18','VarName19','VarName20','VarName21','VarName22','VarName23','VarName24','VarName25','VarName26','VarName27','VarName28','VarName29','VarName30','VarName31','VarName32','VarName33','VarName34','VarName35','VarName36','VarName37','VarName38','VarName39','VarName40','VarName41','VarName42','VarName43','VarName44','VarName45','VarName46','VarName47','VarName48','VarName49','VarName50','VarName51','VarName52','VarName53','VarName54','VarName55','VarName56','VarName57','VarName58','VarName59','VarName60','VarName61','VarName62','VarName63','VarName64','VarName65','VarName66','VarName67','VarName68','VarName69','VarName70','VarName71','VarName72','VarName73','VarName74','VarName75','VarName76','VarName77','VarName78','VarName79','VarName80','VarName81','VarName82','VarName83','VarName84','VarName85','VarName86','VarName87','VarName88','VarName89','VarName90','VarName91'});
 
@@ -129,12 +35,9 @@ clearvars filename delimiter startRow formatSpec fileID dataArray ans;
 
 %% Segregate by Units
 
-
 Chan = UNITS(:,2); % Chan here refers to a single tetrode (data from 4 closely positioned channels)
 Chan = table2array(Chan);
 Chan_num = max(Chan);
-
-
 
 for i = 1:Chan_num
     
@@ -148,22 +51,17 @@ U = table2array(U); % change unit index to array data
 U_num = max(U); % how many units are in the channel?
 
 
-for j = 1:U_num
-
-    TU = find(U == j); % row for each unit
-    
-    MU{j,i} = TT(TU,:); % extract single unit data from single channel data, put them into multi unit data in order
-    
-     
-end
-
+    for j = 1:U_num
+        TU = find(U == j); % row for each unit
+        MU{j,i} = TT(TU,:); % extract single unit data from single channel data, put them into multi unit data in order
+    end
 end
 
 Filename = strsplit(FileName,'.');
 Filename = strcat(Filename(1),'_UNITS');
 save([PathName,Filename{1}],'MU')
-%% Look into each unit
 
+%% Look into each unit
 Unitname = strsplit(FileName,'.');
 Unitname = Unitname(1);
 d = 0;
@@ -191,7 +89,7 @@ end
 
 end
 
-fprintf('----------A1_GR_UNITS_read_v2----------\n');
+fprintf('----------A1_GR_UNITS_read----------\n');
 fprintf('%s 위치에 \n총 %d 개의 파일이 생성되었습니다.\n',PathName,numunit);
 
     
--- GR7/A2_GR_singleUnit_anlyzer.m
+++ GR7/A2_GR_singleUnit_anlyzer.m
@@ -1,6 +1,5 @@
 %% Unit basic analysis
 
-
 if ~exist('f','var')
     [FileName,PathName] = uigetfile;
 
@@ -9,72 +8,52 @@ if ~exist('f','var')
     load(filepath);
 end
 
+clearvars FileName PathName filepath
+
 %% waveform
 
 su = table2array(SU);
+ts = su(:,1); %Timestamps : 언제 firing 했는지를 알려줌.
 
-wav = su(:,4:end);
-
-l = size(wav,1);
-p = size(wav,2)/4;
-
-
-
-
-z = zeros(l,10);
-
-wave1 = wav(:,1:p);
-wave2 = wav(:,p+1:2*p);
-wave3 = wav(:,2*p+1:3*p);
-wave4 = wav(:,3*p+1:4*p);
-
-
-mwave1 = mean(wave1);
-mwave2 = mean(wave2);
-mwave3 = mean(wave3);
-mwave4 = mean(wave4);
-
-pwave1 = wave1';
-pwave2 = wave2';
-pwave3 = wave3';
-pwave4 = wave4';
-
-if ~exist('donotdrawA2','var')
+%% Draw Graph
+if ~exist('donotdrawA2','var') %donotdrawA2 라는 변수가 있는 경우 그래프를 그리지 않음.
     figure(8);
     clf;
-    subplot(2,1,1)
+    subplot(2,1,1);
+    wav = su(:,4:end);
+    p = size(wav,2)/4;
     x = 1:1:p;
-    plot(x,pwave1,'b')
-    hold on
-    plot(x+p,pwave2,'b')
-    plot(x+2*p,pwave3,'b')
-    plot(x+3*p,pwave4,'b')
+    
+    wave1 = wav(:,1:p);
+    wave2 = wav(:,p+1:2*p);
+    wave3 = wav(:,2*p+1:3*p);
+    wave4 = wav(:,3*p+1:4*p);
 
-    ylabel('MicroV')
+    mwave1 = mean(wave1);
+    mwave2 = mean(wave2);
+    mwave3 = mean(wave3);
+    mwave4 = mean(wave4);
 
-    subplot(2,1,2)
+    pwave1 = wave1';
+    pwave2 = wave2';
+    pwave3 = wave3';
+    pwave4 = wave4';
+    
+    plot(x,pwave1,'b');
+    hold on;
+    plot(x+p,pwave2,'b');
+    plot(x+2*p,pwave3,'b');
+    plot(x+3*p,pwave4,'b');
 
-    plot(x,mwave1,'b','LineWidth',2)
-    hold on
-    plot(x+p,mwave2,'b','LineWidth',2)
-    plot(x+2*p,mwave3,'b','LineWidth',2)
-    plot(x+3*p,mwave4,'b','LineWidth',2)
+    ylabel('MicroV');
 
+    subplot(2,1,2);
 
-    ylabel('MicroV')
+    plot(x,mwave1,'b','LineWidth',2);
+    hold on;
+    plot(x+p,mwave2,'b','LineWidth',2);
+    plot(x+2*p,mwave3,'b','LineWidth',2);
+    plot(x+3*p,mwave4,'b','LineWidth',2);
+
+    ylabel('MicroV');
 end
-
-%% Timesptamps
-
- ts = su(:,1);
-% 
-% subplot(4,1,3)
-% line([ts ts],[0 1],'Color','b')
-
-%% FR
-% 
-% subplot(4,1,4)
-% kkk =  histogram(ts, 'BinWidth',60)
-% ylabel('Firing Rate')
-
-
