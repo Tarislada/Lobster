17a471ea25507015943dbbc3922fbeb11ac018ab
Knowblesse <knowblesse@gmail.com>
2018-09-16 13:47:16 +0900

    minor comment changed

--- MatlabCode/Spike/WIP/GaussianKernelBin_diffsize.m
+++ MatlabCode/Spike/WIP/GaussianKernelBin_diffsize.m
@@ -2,12 +2,10 @@
 % Using "Neurons" variable from <LoadAlginedData.m>, generate matrix with 
 %% PARAMETERS
 TIMEWINDOW_BIN = 0.05; % TIMEWINDOW의 각각의 bin 크기는 얼마로 잡을지.
-stdev = 150; % ms
+fs = 1000; % spike를 담을 timeseries의 Fs
+stdev = 150; % std of Gausswin (in ms)
 kernel_size = 1500;
 bin_size = 20;
-
-%% LoadAlignedData
-% Align된 뉴런 데이터를 모두 모아서 하나의 matrix로 만듦.
 
 %% 분석할 .mat 파일 선택
 [filename, pathname] = uigetfile('.mat', 'MultiSelect', 'on');
@@ -22,7 +20,7 @@ end
 
 numNeuron = numel(Paths); % 선택한 뉴런의 수.
 
-%% 선택한 .mat 파일의 수를 가지고 cell을 만든뒤 하나씩 로드하여 저장.
+%% Load Unit.mat file and load into <Neurons>
 Neurons = cell(numNeuron,1); % 로드한 데이터를 담아두는 변수.
 for f = 1 : numNeuron
     load(Paths{f});
@@ -30,22 +28,27 @@ for f = 1 : numNeuron
 end
 clearvars f;
 
-%% Find data parameters
+%% Find Data Parameters
 numTrial = numel(Neurons{1});
-
 OUTPUT = cell(numNeuron,numTrial);
-bdl = zeros(numTrial,1);
+bdl = zeros(numTrial,1); % binned data length
+
+%% Convolve Kernel
 for n = 1 : numel(Neurons)
     for t = 1 : numTrial
-        % 전체 Trial 의 시간을 구하고 이를 기반으로 array를 만듦.
+        % Generate psudo-timeseries data 
+        % with length of window(t,1) ~ window(t,2)
+        % fs : 1000
         tempdata = zeros(round(window(t,2)*1000) - round(window(t,1)*1000),1);
-        % firing 시간에 맞춰서 가장 가까운 timepoint에 1을 배당
+        % assign "1" to spike point 
         tempdata(round(Neurons{n}{t} * 1000) - round(window(t,1)*1000)+1) = 1;
-        % 가우시안 커널과 convolution
+        % Generate gaussian kernel
         kernel = gausswin(kernel_size,kernel_size/(2*stdev));
+        % Convolve gaussian kernel
         tempdata = conv(tempdata,kernel);
+        % Trim start/end point of the data to match the size
         tempdata = tempdata(kernel_size/2+1:end-kernel_size/2+1);
-        % bin으로 나눠서 해당 빈 안에 들어가는 데이터는 전부 합치고 OUTPUT에 대입
+        % binning
         binned_data_length = floor(numel(tempdata) / bin_size);
         bdl(t) = binned_data_length;
         OUTPUT{n,t} = sum(reshape(tempdata(1 : bin_size * binned_data_length),bin_size,binned_data_length),1).^0.5;
