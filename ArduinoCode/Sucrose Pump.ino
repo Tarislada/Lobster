f5b8f9ea8f339c42cb89d29936f5c98d6eedf304
Knowblesse <knowblesse@gmail.com>
2018-11-27 15:56:47 +0900

    Sucrose Pump.ino WIP

--- ArduinoCode/Sucrose Pump.ino
+++ ArduinoCode/Sucrose Pump.ino
@@ -1,25 +1,15 @@
-
-
-
 int test_pin = 7;
-
-
 int valve = 2;
-
-
-
 int LickPin = 12;
 int Lick_TDT = 11;
-
 int Lick_trial_key = 3;
 
 
+bool Lick_trial = false;
 
-boolean Lick_trial = false;
+bool lick_state = true;  // true false reversed! because of sensor specification
 
-boolean lick_state = true;  // true false reversed! because of sensor specification
-
-boolean test_state = false;
+bool test_state = false;
 
 unsigned long currentT = 0;
 unsigned long maxlick = 6000;
@@ -29,22 +19,14 @@ unsigned long suppress = 3000;
 int valve_inhibit = 0;
 int valv_i = 5; //valve inhibit key
 
-
-
-
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
   
-  
-  
- pinMode(test_pin, INPUT);
+  pinMode(test_pin, INPUT);
   pinMode(valve, OUTPUT);
-
-
   pinMode(LickPin, INPUT);
-
   pinMode(Lick_TDT, OUTPUT); 
-
   pinMode(Lick_trial_key, OUTPUT);
   pinMode(valv_i, INPUT); 
 
@@ -52,50 +34,42 @@ void setup() {
 
 
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+  currentT = millis();
+
+  lick_state = digitalRead(LickPin);
+  test_state = digitalRead(test_pin);
+  valve_inhibit = digitalRead(valv_i);
 
 
-currentT = millis();
-
-
-
-lick_state = digitalRead(LickPin);
-test_state = digitalRead(test_pin);
-valve_inhibit = digitalRead(valv_i);
-
-
-if (lick_state == false)
-{ digitalWrite(Lick_TDT, HIGH);
-  if(valve_inhibit == 0)
-  {
-  digitalWrite(valve, HIGH);
+  if (lick_state == false)
+  { 
+    digitalWrite(Lick_TDT, HIGH);
+    
+    if(valve_inhibit == 0)
+    {
+      digitalWrite(valve, HIGH);
+    }
+    digitalWrite(Lick_trial_key,HIGH);
   }
-  digitalWrite(Lick_trial_key,HIGH);
-   }
+
+  if (lick_state == true)
+  { 
+    digitalWrite(Lick_TDT, LOW);
+    digitalWrite(valve, LOW);
+    digitalWrite(Lick_trial_key,LOW);
 
 
-if (lick_state == true)
-{ digitalWrite(Lick_TDT, LOW);
-digitalWrite(valve, LOW);
-digitalWrite(Lick_trial_key,LOW);
+    if (test_state == true)
+    { 
+      digitalWrite(valve, HIGH);
+    }
 
-
-if (test_state == true)
-{ 
-  digitalWrite(valve, HIGH);
-      }
-
-
-if (test_state == false)
-{ 
-digitalWrite(valve, LOW);
-}
-
-
-}
-
-
-
-
+    if (test_state == false)
+    { 
+      digitalWrite(valve, LOW);
+    }
+  }
 }
