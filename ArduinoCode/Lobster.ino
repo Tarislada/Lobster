02ba55b858155a13c5dd97c270db856d7d38ae62
Knowblesse <knowblesse@gmail.com>
2019-08-08 10:22:46 +0900

    Lobsterbot bug fixed
    - no jittering of the left arm
    - HF noise removed
    - arms are

--- ArduinoCode/Lobster.ino
+++ ArduinoCode/Lobster.ino
@@ -1,88 +1,68 @@
 #include <Servo.h> 
-#include <Arduino.h>
-
-// two HM-1301W motors
-Servo myservo1;
-Servo myservo2;
  
-const int PIN_ATTACK_OUT = 12;
-const int PIN_PURGE_IN = 2;
-const int PIN_ATTACK_ORDER_IN = 4;
+const int PIN_ATTACK_IN = 9;
+const int PIN_ATTACK_OUT = 10;
 
-const int PIN_SERVO1 = 9;
-const int PIN_SERVO2 = 6;
+const int PIN_SERVO1 = 12; // Left 
+const int PIN_SERVO2 = 11; // Right
 
 void setup() 
 {
-  pinMode(PIN_ATTACK_ORDER_IN, INPUT);
+  pinMode(PIN_ATTACK_IN, INPUT);
   pinMode(PIN_ATTACK_OUT, OUTPUT);
-  digitalWrite(PIN_ATTACK_OUT,LOW);
-  
-  pinMode(PIN_PURGE_IN, INPUT);
 
-  myservo1.attach(PIN_SERVO1);
-  myservo2.attach(PIN_SERVO2);  
+  digitalWrite(PIN_ATTACK_OUT,LOW);
+
+  Serial.begin(9600);
+
+  // initialize motor
+  setServoAngle(PIN_SERVO1, 60);
+  setServoAngle(PIN_SERVO2, 145);
 } 
  
 void loop() 
 {
-
-  fixState = 
-
-
-  if(digitalRead(PIN_ATTACK_ORDER_IN) == LOW) // calm
-  {
-    digitalWrite(PIN_ATTACK_OUT,LOW);
-    // Initial State : sets the servo position according to the scaled value
-    myservo1.write(60);
-    myservo2.write(145);
-  }
-  else // attack!
-  {
-    // Send Attack Info
+  if(digitalRead(PIN_ATTACK_IN) == HIGH) // Attack
+    {
+    // Send Attack Info to TDT
     digitalWrite(PIN_ATTACK_OUT,HIGH);
 
     // Perform Attack
-    myservo1.write(105);               
-    myservo2.write(105);
+    attack();
+
+    // Send Attack Info
+    digitalWrite(PIN_ATTACK_OUT,LOW);
+  }
+}
+
+void setServoAngle(int pin, int angle)
+{
+  Servo s;
+  s.attach(pin);
+  s.write(angle);
+  delay(100);
+  s.detach();
+}
+
+void attack()
+{
+    Servo myservo1;
+    Servo myservo2;
+    myservo1.attach(PIN_SERVO1);
+    myservo2.attach(PIN_SERVO2);
+
+    myservo1.write(90);               
+    myservo2.write(115);
     delay(170);
     myservo1.write(60);  
     myservo2.write(145);
     delay(230);
-    myservo1.write(105);               
-    myservo2.write(105);
+    myservo1.write(90);               
+    myservo2.write(115);
     delay(170);
     myservo1.write(60);
     myservo2.write(145);
     delay(230);
-
-    // Send Attack Info
-    digitalWrite(attackPin,LOW);
-  }
-
-  // Purge Motor incase of stuck
-  if(digitalRead(PIN_PURGE_IN) == HIGH)
-  {
-    for(int pos1 = 60; pos1 <= 85; pos1 += 1) 
-    {
-      myservo1.write(pos1);
-      delay(15);
-    }
-    for(int pos1 = 85; pos1 >= 60; pos1 -= 1) 
-    {
-      myservo1.write(pos1);
-      delay(15);
-    }
-    for(int pos2 = 145; pos2 >= 125; pos2 -= 1)
-    {
-      myservo2.write(pos2);
-      delay(15);
-    }
-
-    for (pos2 = 125; pos2 <= 145; pos2 += 1)
-    {
-      myservo2.write(pos2);
-      delay(15);
-    }
-  }
+    myservo1.detach();
+    myservo2.detach();
 }
