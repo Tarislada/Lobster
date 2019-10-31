b9f25b0380da80e53ef479c130db5b4e7dddd981
Knowblesse <knowblesse@gmail.com>
2019-10-31 14:11:54 +0900

    sucrose pump dead code erased

--- ArduinoCode/Sucrose Pump.ino
+++ ArduinoCode/Sucrose Pump.ino
@@ -3,20 +3,14 @@ const int PIN_LICK_TRIAL_KEY_OUTPUT = 6;
 const int PIN_LICK_TDT_OUTPUT = 7;
 
 const int PIN_CONTROL_LICK_INPUT = 8; // previously Valve Inhibit. Manual Button at Control panel
-const int PIN_MANUAL_LICK_INPUT = 9; // 
 const int PIN_LICKPIN_INPUT = 11;
 
 const int PIN_POWER_ON_OUTPUT = 12;
-const int PIN_VALVE_ON_OUTPUT = 13;
+const int PIN_VALVE_ON_LED_OUTPUT = 13;
 
 bool Lick_trial = false;
 bool isLickIRBlocked = true;
 bool isManualButtonPushed = false;
-
-unsigned long currentT = 0;
-unsigned long maxlick = 6000;
-unsigned long flick = 0;
-unsigned long suppress = 3000;
 
 void setup() 
 { 
@@ -25,9 +19,8 @@ void setup()
   pinMode(PIN_LICK_TDT_OUTPUT, OUTPUT); 
   pinMode(PIN_LICK_TRIAL_KEY_OUTPUT, OUTPUT);
   pinMode(PIN_CONTROL_LICK_INPUT, INPUT);
-  pinMode(PIN_MANUAL_LICK_INPUT, INPUT); 
   pinMode(PIN_POWER_ON_OUTPUT, OUTPUT);
-  pinMode(PIN_VALVE_ON_OUTPUT,OUTPUT);
+  pinMode(PIN_VALVE_ON_LED_OUTPUT,OUTPUT);
 
   digitalWrite(PIN_POWER_ON_OUTPUT, HIGH);
 }
@@ -35,17 +28,15 @@ void setup()
 
 void loop() 
 {
-  currentT = millis();
 
   isLickIRBlocked = !digitalRead(PIN_LICKPIN_INPUT);
   isManualButtonPushed = digitalRead(PIN_CONTROL_LICK_INPUT);
-
 
   if (isLickIRBlocked) // sensor blocked = licking
   { 
     digitalWrite(PIN_LICK_TDT_OUTPUT, HIGH);
     digitalWrite(PIN_VALVE_OUTPUT, HIGH);
-    digitalWrite(PIN_VALVE_ON_OUTPUT,HIGH);
+    digitalWrite(PIN_VALVE_ON_LED_OUTPUT,HIGH);
     digitalWrite(PIN_LICK_TRIAL_KEY_OUTPUT,HIGH);
   }
   else // sensor not blocked = not licking
@@ -54,15 +45,15 @@ void loop()
     digitalWrite(PIN_LICK_TRIAL_KEY_OUTPUT, LOW);
     digitalWrite(PIN_VALVE_OUTPUT, LOW);
 
-    if (isManualButtonPushed || digitalRead(PIN_MANUAL_LICK_INPUT))
+    if (isManualButtonPushed)// override my manual button
     { 
       digitalWrite(PIN_VALVE_OUTPUT, HIGH);
-      digitalWrite(PIN_VALVE_ON_OUTPUT,HIGH);
+      digitalWrite(PIN_VALVE_ON_LED_OUTPUT,HIGH);
     }
     else
     { 
       digitalWrite(PIN_VALVE_OUTPUT, LOW);
-      digitalWrite(PIN_VALVE_ON_OUTPUT,LOW);
+      digitalWrite(PIN_VALVE_ON_LED_OUTPUT,LOW);
     }
   }
 }
